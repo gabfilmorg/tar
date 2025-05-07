e95dce8073f71ddcefde923b38a233bfb5c94ebf
gabfilmorg <gab.silverio@hotmail.com>
2025-05-07 15:06:49 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+}
+
+module.exports = nextConfig 
